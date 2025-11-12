45075050a31de0aa58b54cea7c0a6721567ea634
Helena <helenavivasramajo98@gmail.com>
2025-11-12 18:09:33 +0100

    Register

--- Front/src/pages/Register.jsx
+++ Front/src/pages/Register.jsx
@@ -8,8 +8,8 @@ const Register = () => {
             <Header />
             <div className="flex min-h-[80vh] w-full items-center justify-center p-6 md:p-10">
                 <div className="w-full max-w-sm">
-            <SignupForm />
-            </div>
+                    <SignupForm />
+                </div>
             </div>
             <Footer />
         </div>
