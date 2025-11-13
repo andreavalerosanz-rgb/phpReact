aa53db380afe400e09f188fa26fa019f04e48c75
Helena <helenavivasramajo98@gmail.com>
2025-11-13 19:20:20 +0100

    Merge finalizado: App.jsx limpio y rutas unificadas

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react'
+import NuevaReservaDashboard from './pages/NuevaReservaDashboard.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NuevaReservaTraslado from './pages/NuevaReservaTraslado.jsx'
 import Login from './pages/Login.jsx'
 import Home from './pages/Home.jsx'
 import Register from './pages/Register.jsx'
 import Calendario from './pages/Calendario.jsx'
-<<<<<<< HEAD
-import NuevaReserva from './pages/NuevaReserva.jsx'
-=======
 import Dashboard from './pages/Dashboard.jsx'
 import PerfilUsuario from './pages/PerfilUsuario.jsx'
 import MisReservas from "./pages/MisReservas"
-import NuevaReserva from "./pages/NuevaReserva"
 import DetalleReserva from "./pages/DetalleReserva"
->>>>>>> origin/dashboard
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
@@ -24,17 +21,13 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Register />} />
           <Route path="/calendario" element={<Calendario />} />
-<<<<<<< HEAD
-          <Route path='/nueva-reserva' element={<NuevaReserva />} />
-=======
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/perfil" element={<PerfilUsuario />} />
           <Route path="/reservas" element={<MisReservas />} />
-          <Route path="/reservas/nueva" element={<NuevaReserva />} />
+          <Route path="/reservas/nueva" element={<NuevaReservaDashboard />} />
           <Route path="/reservas/:id" element={<DetalleReserva />} />
 
 
->>>>>>> origin/dashboard
         </Routes>
       </div>
     </Router>
